8cb1c99e9f39510c8e1338c9e25624d62b3e497b
Ezequiel Lozano <lozano.ez@gmail.com>
2021-01-16 12:34:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# GADGET Plotting
+
+Script for the generation of figures, GIFs and videos from the data produce by a GAGET3/4 simulation.
+
+- It only works with the traditional output format (binary data) which is the default in GADGET3 (SnapFormat=1) and a compatibility option in GADGET4 (legacy format selected with SnapFormat=1 too).
+- It is just an script intended to be imported as is, it is not a module nor a package.
+- A small testing data set is provided in testing/test_snapshots/.
+- The testing script testing/testing.jl shows examples of every function, how to import the script, and provides a sanity check, as it should run without errors.
+- A testing/Manifest.toml and testing/Project.toml files provide the dependencies.
+
+## Functions
+
+There are four tiers of functions:
+
+- AUXILIARY FUNCTIONS: This are only for internal used, and compensate lack of functionality in Base or the used libraries, e.g. Unitful.jl, or isolate a bit a repeated logic.
+- DATA ACQUISITION FUNCTIONS: This are only for internal used. They take the raw data, applied some transformation and return it as familiar data estructures.
+- PLOTTING FUNCTIONS: This functions take data in the format outputted by the data acquisition functions and return plot objects.
+- PIPELINE FUNCTIONS: This are the ones intended to be externally used. They take the location of the snapshot files and configuration parameters to produce a series of figures/gif/videos automatically.
+
+NOTE: Some of the pipeline functions may produce by default a large number of images (but it can be configure to do less). And they may take a long time to run, especially if the function utilices the pgfplotsx backend of Plots.jl.
+
+## Documentation
+
+Each function is documented within the script, where the docstring explains each function, its arguments and its returns.
+For examples refer to testing/testing.jl, note that it expect a particular structure of its files, namely:
+
+.
+│
+├── gadget_plotting.jl
+│
+└── testing
+	│
+    ├── results
+	│	│
+    │	├── ...
+    │	└── ...
+	│
+    └── testing.jl
+	│	
+	└── Manifest.toml
+	│	
+	└── Project.toml
+
+
+Which is the one present in the repo.
+
+### References
+
+[GADGET2](https://wwwmpa.mpa-garching.mpg.de/gadget/)
+[GADGET4](https://wwwmpa.mpa-garching.mpg.de/gadget4/)
+
+### No guarantee
+
+This script may break at any moment, and some functions are intended for data produce by GADGET3 which is not a public code. So no guaranties are given.
+
+
---
+++ testing/testing.jl
@@ -0,0 +1,261 @@
+########################################################################################
+# Testing script for gadget_plotting.jl, it can be run as is, and it shouldn't trow any errors.
+# It expects to find gadget_plotting.jl one directory up, and a directory named results/,
+# for storing the testing files, in its same directory.
+########################################################################################
+
+include("../gadget_plotting.jl")
+
+"Base path for the directories where the figures and animations will be saved."
+const BASE_OUT_PATH = "results/"  
+
+"Directory containing the snapshot files."   
+const SNAP_PATH = "test_snapshots/"  
+
+"Base name of the snapshot files."
+const SNAP_NAME = "snap" 
+
+"Side dimension of the simulated region, with units, for the case of vacuum boundary conditions."
+const BOX_SIZE = 200 * UnitfulAstro.kpc 
+
+"Value of ComovingIntegrationOn: 0 -> Newtonian simulation, 1 -> Cosmological simulation."               
+const SIM_COSMO = 0 
+
+"Frame rate for the animations."                   
+const FPS = 4    
+
+########################################################################################
+# TEST OF DATA ACQUISITION FUNCTIONS.
+########################################################################################
+
+snaps = getSnapshots(SNAP_NAME, SNAP_PATH)
+snap_files = snaps["snap_files"]
+display(snaps)
+println()
+
+time_series = timeSeriesData(snap_files, sim_cosmo=SIM_COSMO)
+display(time_series)
+println()
+
+pos = positionData( snap_files[21], 
+                    sim_cosmo=SIM_COSMO, 
+                    length_unit=UnitfulAstro.Mpc, 
+                    box_size=BOX_SIZE)
+display(pos)
+println()
+
+density = densityData(snap_files[21], sim_cosmo=SIM_COSMO)
+display(density)
+println()
+
+hsml = hsmlData(snap_files[21], sim_cosmo=SIM_COSMO)
+display(hsml)
+println()
+
+gas_mass = massData(snap_files[21], "gas", sim_cosmo=SIM_COSMO)
+display(gas_mass)
+println()
+
+dm_mass = massData(snap_files[21], "dark_matter", sim_cosmo=SIM_COSMO)
+display(dm_mass)
+println()
+
+star_mass = massData(snap_files[21], "stars", sim_cosmo=SIM_COSMO)
+display(star_mass)
+println()
+
+gas_z = zData(snap_files[21], "gas", sim_cosmo=SIM_COSMO)
+display(gas_z)
+println()
+
+sfrtxtdata = sfrTxtData(SNAP_PATH, SNAP_NAME, sim_cosmo=SIM_COSMO)
+display(sfrtxtdata)
+
+# #######################################################################################
+# TEST OF PLOTTING FUNCTIONS.
+# #######################################################################################
+
+scatterGridPlot(pos)
+savefig(BASE_OUT_PATH * "test_scatterGridPlot.png")
+
+densityMapPlot(pos, gas_mass, hsml, density, plane="XY")
+savefig(BASE_OUT_PATH * "test_densityMapPlot_XY.png")
+
+densityMapPlot(pos, gas_mass, hsml, density, plane="All")
+savefig(BASE_OUT_PATH * "test_densityMapPlot_All.png")
+
+color = [:batlow, :bone, :CMRmap, :grayC, :inferno, :seaborn_rocket_gradient, :YlOrRd_9]
+
+for c in color
+    densityMapPlot(pos, gas_mass, hsml, density, plane="All", color=c)
+    savefig(BASE_OUT_PATH * "test_densityMapPlot_" * string(c) * ".png")
+end
+
+gasStarEvolutionPlot(time_series, pos, 21)
+savefig(BASE_OUT_PATH * "test_gasStarEvolutionPlot.png")
+
+pgfplotsx()
+timeSeriesPlot(time_series, mass_factor=0, number_factor=4)
+savefig(BASE_OUT_PATH * "test_timeSeriesPlot.png")
+
+scaleFactorSeriesPlot(time_series, mass_factor=0, number_factor=4)
+savefig(BASE_OUT_PATH * "test_scaleFactorSeriesPlot.png")
+
+redshiftSeriesPlot(time_series, mass_factor=0, number_factor=4)
+savefig(BASE_OUT_PATH * "test_redshiftSeriesPlot.png")
+
+compareSimulationsPlot( "star_mass", 
+                        "sfr", 
+                        ["sim1" "sim2"], 
+                        (time_series, time_series); 
+                        title="SFMS relation", 
+                        x_factor=10,
+                        log_scale=true) 
+savefig(BASE_OUT_PATH * "test_compareSimulationsPlot.png")
+
+densityHistogramPlot(density, 1 * UnitfulAstro.Myr, factor=10)
+savefig(BASE_OUT_PATH * "test_densityHistogramPlot.png")
+
+densityProfilePlot(pos, gas_mass,  1 * UnitfulAstro.Myr, bins=50, factor=13)
+savefig(BASE_OUT_PATH * "test_densityProfilePlot.png")
+
+metallicityProfilePlot(pos, gas_mass, gas_z, 1 * UnitfulAstro.Myr, bins=100)
+savefig(BASE_OUT_PATH * "test_metallicityProfilePlot.png")
+
+sfrTxtPlot(SNAP_PATH, SNAP_NAME, 1, [4, 6], title="run_A_01", bins=50, scale=[:identity, :log10], sim_cosmo=SIM_COSMO)
+savefig(BASE_OUT_PATH * "test_sfrTxtPlot.png")
+
+########################################################################################
+# TEST OF PIPELINE FUNCTIONS.
+########################################################################################
+
+gr()
+scatterGridPipeline(SNAP_NAME, 
+                    SNAP_PATH, 
+                    BASE_OUT_PATH, 
+                    "scatter_anim", 
+                    FPS,
+                    sim_cosmo=SIM_COSMO,
+                    length_unit=UnitfulAstro.Mpc,
+                    region_size=BOX_SIZE)
+
+densityMapPipeline( SNAP_NAME, 
+                    SNAP_PATH, 
+                    BASE_OUT_PATH, 
+                    "density_anim", 
+                    FPS,
+                    plane="All",
+                    sim_cosmo=SIM_COSMO,
+                    region_size=BOX_SIZE)
+
+gasStarEvolutionPipeline(   SNAP_NAME,
+                            SNAP_PATH, 
+                            BASE_OUT_PATH,
+                            "gas_star_evolution",
+                            FPS,
+                            sim_cosmo=SIM_COSMO,
+                            region_size=BOX_SIZE)
+
+pgfplotsx()
+evolutionSummaryPipeline(   SNAP_NAME, 
+                            SNAP_PATH,  
+                            BASE_OUT_PATH, 
+                            "evolution_summary",
+                            sim_cosmo=SIM_COSMO,
+                            mass_factor=10, 
+                            number_factor=4)
+
+compareSimulationsPipeline( [SNAP_PATH, SNAP_PATH],
+                            BASE_OUT_PATH,
+                            [SNAP_NAME, SNAP_NAME],   
+                            ["sim1" "sim2"], 
+                            "compare",
+                            "star_mass",
+                            "sfr",
+                            x_factor=10,
+                            title="SFMS relation")
+
+densityHistogramPipeline(   SNAP_NAME, 
+                            SNAP_PATH, 
+                            BASE_OUT_PATH, 
+                            "density_histogram_anim", 
+                            FPS,
+                            sim_cosmo=SIM_COSMO,
+                            factor=10)
+
+densityProfilePipeline( SNAP_NAME, 
+                        SNAP_PATH, 
+                        BASE_OUT_PATH, 
+                        "density_profile_anim", 
+                        FPS,
+                        "gas";
+                        bins=80,
+                        factor=6,
+                        region_factor=0.125,
+                        sim_cosmo=SIM_COSMO,
+                        region_size=BOX_SIZE)
+
+metallicityProfilePipeline( SNAP_NAME, 
+                            SNAP_PATH, 
+                            BASE_OUT_PATH, 
+                            "metallicity_profile_anim", 
+                            FPS,
+                            "gas";
+                            bins=80,
+                            region_factor=5.0,
+                            sim_cosmo=SIM_COSMO,
+                            region_size=BOX_SIZE)
+
+########################################################################################
+# TEST OF AUXILIARY FUNCTIONS.
+########################################################################################
+
+println(uglyUnitDeserialization(UnitfulAstro.Msun))
+println(uglyUnitDeserialization(UnitfulAstro.Myr))
+println(uglyUnitDeserialization(UnitfulAstro.kpc))
+
+fig2D = plot(rand(100))
+println(relative(fig2D, 0.5, 0.5))
+fig3D = surface(rand(100, 100))
+println(relative(fig3D, 0.5, 0.5, 0.5))
+
+makeVideo(BASE_OUT_PATH * "scatter/images", ".png", BASE_OUT_PATH, "test_video", FPS)
+
+x_data = [1:1000...]
+y_data = rand(1000)
+x_smooth, y_smooth = smoothWindow(x_data, y_data, 50)
+plot(x_data, y_data, seriestype=:scatter, legend=false)
+plot!(x_smooth, y_smooth, seriestype=:line, lw=3)
+savefig(BASE_OUT_PATH * "test_smoothWindow.png")
+
+positions = pos["gas"]
+distances = sqrt.(positions[:,1].^2 .+ positions[:,2].^2 .+  positions[:,3].^2)
+box_size = ustrip(Float64, unit(BOX_SIZE), BOX_SIZE)
+r, ρ = densityProfile(gas_mass["mass"], distances, box_size, 80)
+plot(   r, ρ, 
+        lw=3, 
+        xlabel="Distance / $(pos["unit"])", 
+        ylabel="ρ / $(gas_mass["unit"]) $(pos["unit"])^(-3)", 
+        legend=false)
+savefig(BASE_OUT_PATH * "test_densityProfile.png")
+
+positions = pos["gas"]
+distances = sqrt.(positions[:,1].^2 .+ positions[:,2].^2 .+  positions[:,3].^2)
+size = ustrip(Float64, unit(BOX_SIZE), BOX_SIZE)
+r, z = metallicityProfile(gas_mass["mass"], distances, gas_z["Z"], box_size, 80)
+plot(r, z, lw=3, xlabel="Distance / $(pos["unit"])", ylabel="Z / Zsun", legend=false)
+savefig(BASE_OUT_PATH * "test_metallicityProfile.png")
+
+########################################################################################
+# EXTRA TESTS.
+# This functions when executed should trow an error.
+########################################################################################
+
+# massData(snap_files[21], "unobtainium", sim_cosmo=SIM_COSMO)
+# zData(snap_files[21], "unobtainium", sim_cosmo=SIM_COSMO)
+
+########################################################################################
+# DELETE ALL GENERATED TESTING FILES.
+########################################################################################
+
+# rm(BASE_OUT_PATH, recursive=true)
