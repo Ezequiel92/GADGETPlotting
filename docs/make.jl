ceba595f9d275e61390b188be1f7d88536b3f4b1
Ezequiel Lozano <elozano@df.uba.ar>
2025-06-30 23:16:59 -0300

    Update docs theme

--- docs/make.jl
+++ docs/make.jl
@@ -5,27 +5,22 @@ using DocumenterTools: Themes
 # True if being deployed, false if being compile locally
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 
-# Compile themes
-Themes.compile(
-    joinpath(@__DIR__, "src/assets/geostats-light.scss"), 
-    joinpath(@__DIR__, "src/assets/themes/documenter-light.css"),
-)
-Themes.compile(
-    joinpath(@__DIR__, "src/assets/geostats-dark.scss"), 
-    joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"),
-)
+# # Compile themes
+# Themes.compile(
+#     joinpath(@__DIR__, "src/assets/geostats-light.scss"),
+#     joinpath(@__DIR__, "src/assets/themes/documenter-light.css"),
+# )
+# Themes.compile(
+#     joinpath(@__DIR__, "src/assets/geostats-dark.scss"),
+#     joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"),
+# )
 
 # Compile documentation
 makedocs(
     sitename="GADGETPlotting.jl",
     authors = "Ezequiel Lozano",
     format = Documenter.HTML(
-        assets=[
-            asset(
-                "https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", 
-                class=:css,
-            )
-        ],
+        assets=["./assets/GI-docs.css"],
         prettyurls = CI,
         edit_link = "main",
     ),
@@ -46,5 +41,5 @@ if CI
         repo = "github.com/Ezequiel92/GADGETPlotting.git",
         devbranch = "main",
         versions = ["dev" => "dev"],
-    ) 
+    )
 end
