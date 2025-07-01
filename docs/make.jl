ecb87ad3711919d8db22631d259d8e317f6ed607
Ezequiel Lozano <elozano@df.uba.ar>
2025-06-30 23:25:58 -0300

    Final docs theme update

--- docs/make.jl
+++ docs/make.jl
@@ -5,16 +5,6 @@ using DocumenterTools: Themes
 # True if being deployed, false if being compile locally
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 
-# # Compile themes
-# Themes.compile(
-#     joinpath(@__DIR__, "src/assets/geostats-light.scss"),
-#     joinpath(@__DIR__, "src/assets/themes/documenter-light.css"),
-# )
-# Themes.compile(
-#     joinpath(@__DIR__, "src/assets/geostats-dark.scss"),
-#     joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"),
-# )
-
 # Compile documentation
 makedocs(
     sitename="GADGETPlotting.jl",
